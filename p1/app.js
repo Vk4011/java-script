d696e2b2344d771e57f97625c7e7f164087d7098
vikram <maticsystem1@gmail.com>
2023-04-28 15:27:11 +0530

    project Dom

---
+++ p1/app.js
@@ -0,0 +1,10 @@
+
+function night(){
+    document.getElementById("sun").src = "moon.png";
+    document.getElementById("bg").style.backgroundColor=" rgb(25, 29, 45)";
+}
+
+function day(){
+    document.getElementById("sun").src ="sun.png";
+    document.getElementById("bg").style.backgroundColor ="white";
+}
