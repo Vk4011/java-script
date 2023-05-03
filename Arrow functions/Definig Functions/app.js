0ac54a2d481e95db495ea70c24cdf736cc445c10
vikram <maticsystem1@gmail.com>
2023-05-03 08:25:11 +0530

    function Declaration & Expression

---
+++ Arrow functions/Definig Functions/app.js
@@ -0,0 +1,24 @@
+//There are multiple ways to define a function
+//Function Declaration
+//Function Expression
+//Arrow Functions
+//Function Constructors
+
+
+
+//Function Declaration
+
+function add(a,b){
+    return a+b;
+
+}
+console.log(add(4,3));
+
+
+//Function Expression
+
+let mul = function(x,y){
+    return x*y;
+
+};
+console.log(mul(4,3));
