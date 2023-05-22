bd055cb196c9c84effbfe6ded37c730afc9e411c
Vikram <vikramdev772@gmail.com>
2023-05-22 16:30:00 +0530

    Class

---
+++ java-script/class/app.js
@@ -0,0 +1,20 @@
+
+
+// class and objects
+// in oops a class is an extensible program code template for
+// creating objects, providing intial values for state (master variables) and
+
+class Student{
+    submit(){
+        alert("Form submitted")
+    }
+    cance(){
+        alert("Form is canceled")
+    }
+}
+
+let harry = new Student()
+let Ron   = new Student()
+
+harry.submit()
+Ron.submit()
