e9397772557f84d00caaac309a4f830769cfe067
Vk4011 <gvikramram63@gmail.com>
2023-06-06 22:54:45 +0530

    t

---
+++ factory/app.js
@@ -0,0 +1,13 @@
+
+//factory
+
+const personFactory = (name, age) => {
+    const sayHello = () => console.log('hello!');
+    return { name, age, sayHello };
+  };
+  
+  const jeff = personFactory('jeff', 27);
+  
+  console.log(jeff.name); // 'jeff'
+  
+  jeff.sayHello(); // calls the function and logs 'hello!'
